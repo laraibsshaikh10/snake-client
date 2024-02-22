bc2040da25e9d0af467231043e7634f30d0d91b1
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-02-22 03:53:55 -0500

    Added Stdin Setup function

--- play.js
+++ play.js
@@ -3,3 +3,26 @@
 const { connect } = require("./client");
 
 connect;
+
+// setup interface to handle user input from stdin
+//the stdin object returned by setupInput will allow us to listen for keyboard input and react to it.
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+
+    // \u0003 maps to ctrl+c input to exit
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+
+setupInput();
